9f8aa1e583ea6bd86762cae3ce4b0785d6ee167b
HoaNguyen <httt.dev@gmail.com>
2023-01-07 22:06:11 +0700

    GET/POST query params

--- src/index.js
+++ src/index.js
@@ -8,6 +8,15 @@ const { engine } = require("express-handlebars");
 const app = express();
 const port = 3000;
 
+//add them middleware de xu ly cho req.body ( neu khong thi req.body se co tri la undefine)
+//doi voi express version < 4.16 thi chua ho tro middlware nay , luc nay se can install them body-parser
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+
 //static file
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -35,6 +44,19 @@ app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources\\views"));
 
 app.get("/", (req, res) => res.render("home"));
+
+app.get("/search", (req, res) => {
+  //get param : req.query.xxx
+
+  res.render("search");
+});
+
+app.post("/search", (req, res) => {
+  //get data from FormData
+  // req.body  ( can middlware de xu ly  luu data gui len tu client vao bien body)
+
+  res.render("search");
+});
 
 app.listen(port, () =>
   console.log(`App listening at http://localhost:${port}`)
